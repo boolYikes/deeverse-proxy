f883bcdd4e7101fcc797956170e4bce8e113eba9
Dongwon Henry Seon <tunacome@gmail.com>
2025-07-10 12:00:45 +0000

    chore: A-record renewal, cert renewal, bug fix

--- ops.sh
+++ ops.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
+COMPOSE="/lab/dee/repos_side/deeverse_proxy/docker-compose.yaml"
+
 if [ $# -eq 0 ]; then
     echo "Run without args for this man page."
     echo "--ng: Only run nginx."
     echo "--cert: Run certbot and then nginx."
     echo "--cert-only: Run certbot and terminate."
+    echo "--down: Turn it off."
 else
     for arg in "$@"; do
         case "$arg" in
             --ng)
                 echo "Running Nginx straight up!"
-                docker compose up nginx -d
+                docker compose -f "$COMPOSE" up nginx -d
                 ;;
             --cert)
                 echo "Gotta check dem certs. No sweat!"
-                docker compose up certbot -d
-                docker compose up nginx -d
+                docker compose -f "$COMPOSE" up certbot -d
+                docker compose -f "$COMPOSE" up nginx -d
                 ;;
             --cert-only)
                 echo "Renewal only and then terminating..."
-                docker compose up certbot -d
-                docker compose down
+                docker compose -f "$COMPOSE" up certbot -d
+                docker compose -f "$COMPOSE" down certbot
                 ;;
             --down)
                 echo "We have rights to deny services...I think...?"
-                docker compose down
+                docker compose -f "$COMPOSE" down
                 ;;
             *)
                 echo "Unknown argument: $arg" >&2
